3243cc4c7a030a8183837cd8e2dbbe0f654290b8
pavan <56758401+PAVAN1410@users.noreply.github.com>
2021-11-08 13:00:22 +0530

    created division function

--- calc.py
+++ calc.py
@@ -3,6 +3,12 @@ def add(a,b):
 
 def mul(a,b):
     return a*b
+
+
+def div(a,b):
+    return a/b
 print(add(1,26))
 print(mul(10,10))     
 print("added multiplication")
+print(div(10,10))     
+print("added division")
